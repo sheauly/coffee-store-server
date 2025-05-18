b73dbe2032decdfc58543c3e94f98e4bff7d4aac
halimawebd <inthesumon1010@gmail.com>
2025-05-18 16:55:40 +0600

    complete the server site project

--- index.js
+++ index.js
@@ -70,12 +70,38 @@ async function run() {
     })
 
     // user related APIS
+
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray()
+      res.send(result);
+    })
+
+
     app.post('/users', async (req, res) => {
       const userProfile = req.body;
       console.log(userProfile)
       const result = await usersCollection.insertOne(userProfile);
       res.send(result);
 
+    })
+
+    app.patch('/users', async(req, res) => {
+      const { email, lastSignInTime } = req.body;
+      const filter = { email: email }
+      const updatedDoc = {
+        $set: {
+          lastSignInTime: lastSignInTime
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updatedDoc)
+      res.send(result);
+    })
+
+    app.delete('/delete/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
     })
 
     await client.db("admin").command({ ping: 1 });
